51efbfad64d240e90a2f282b7f59a56da621dc27
Donald Whitely <dswhitely1@gmail.com>
2019-06-07 11:43:34 -0400

    Card Functionality Working

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 const Cards = props => {
+  const {cards} = props;
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      {cards.map((card,i)=> <Card card={card} key={i} />)}
     </div>
   )
 }
