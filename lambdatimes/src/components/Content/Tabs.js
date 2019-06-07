89e2d17b19b293505b76dcef9a7a6f7993a09b96
Donald Whitely <dswhitely1@gmail.com>
2019-06-07 11:40:05 -0400

    Tab Functionality Working and Rendering

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Tab from './Tab';
 const Tabs = props => {
+    const {selectedTab, selectedTabHandler, tabs} = props;
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
+
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+          {tabs.map((tab, i) => <Tab tab={tab} selectedTab={selectedTab} selectedTabHandler={selectedTabHandler} key={i} />)}
       </div>
     </div>
   );
